fa23c440aa0720a3675bbdb9bbde894b7de27e82
Viktar Holub <holsedas.java@gmail.com>
2024-02-08 08:20:59 +0300

    first commit

---
+++ src/main/java/com/example/demo/entity/Person.java
@@ -0,0 +1,22 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", unique = true, nullable = false)
+    private String firstName;
+    @Column(name = "last_name", unique = true, nullable = false)
+    private String lastName;
+}
